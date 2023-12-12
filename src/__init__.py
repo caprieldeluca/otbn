a244740df29ae2a13019c77006151698c278cf2f
Gabriel De Luca <caprieldeluca@gmail.com>
2023-12-12 15:35:31 -0300

    Crear estructura del codigo fuente.

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__copyright__ = '(C) 2023 by Gabriel De Luca'
+__email__ = 'caprieldeluca@gmail.com'
+__license__ = 'GPL version 3'
+
+def classFactory(iface):
+    """Factory method for the plugin object."""
+    from otbn.otbn_plugin import OtbnPlugin
+    return OtbnPlugin(iface)
+
